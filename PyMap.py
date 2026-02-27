46b20420d5bfeb3eb9b3bd91bf46c6fb5b8893e9
MOHAMED SABAH <121745431+SabahMuhamed@users.noreply.github.com>
2026-02-27 19:54:49 +0530

    Create PyMap.py

---
+++ PyMap.py
@@ -0,0 +1,39 @@
+import funcs
+
+dl = "=" * 70
+title = "PyMap"
+
+banner = f"""
+{dl}
+{title.center(70)}
+{dl}
+
+"""
+
+options = f"""
+[ShortCommand] [Details] (Command)
+    [0] Scan Specific Port (sp)
+    [1] FTP scan (ftp)
+    [2] SSH Scan (ssh)
+    [3] HTTP Scan (http)
+    [] Exit (exit)
+"""
+
+print(banner)
+print(options)
+
+while True :
+    inp = input("[PyMap]=> ").lower()
+    if inp == "0" or inp == "sp" :
+        funcs.sscan()
+    elif inp == "1" or inp == "ftp" :
+        funcs.fscan(21)
+    elif inp == "2" or inp == "ssh" :
+        funcs.fscan(22)
+    elif inp == "3" or inp == "http":
+        funcs.fscan(80)
+    elif inp == "exit" :
+        break
+    else :
+        print("invalid command !!!")
+        continue
